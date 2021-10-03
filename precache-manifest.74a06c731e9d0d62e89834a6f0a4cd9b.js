2164276b113db4ef9de77c4361ec961b832185e6
joy <joychoudhury318@gmail.com>
2021-10-03 14:59:08 +0530

    Updates

---
+++ precache-manifest.74a06c731e9d0d62e89834a6f0a4cd9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a538df648da1580ce79b1c233ab6bdaa",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "b95742f8c7a71d2286b4",
+    "url": "/Hangman/static/css/main.0998170a.chunk.css"
+  },
+  {
+    "revision": "f3293ff065b04177a034",
+    "url": "/Hangman/static/js/2.0e429a6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hangman/static/js/2.0e429a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95742f8c7a71d2286b4",
+    "url": "/Hangman/static/js/main.92e9e3b7.chunk.js"
+  },
+  {
+    "revision": "69478adb882d8b63b804",
+    "url": "/Hangman/static/js/runtime-main.04637f3a.js"
+  }
+]);
